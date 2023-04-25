575ef798360f840fc4f8ecd91697fcd153c8cea3
ChaCha3088 <cha3088@gmail.com>
2023-04-26 00:38:59 +0900

    [11003] 최솟값 찾기 / 플레티넘5

---
+++ Myeonghun/template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <tuple>
+#include <cmath>
+#include <numeric>
+#include <limits>
+
+#define MOD 1000000007
+using ll = long long;
+#define fastIo cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
+
+using namespace std;
+
+int main() {
+    fastIo;
+
+    //입력
+    FILE *pFile = freopen("input.txt", "r", stdin);
+
+}
